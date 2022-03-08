3d0aa36bf8608d3f8ea4e0b8de36b7695fe62b8e
Sven <SvePu@users.noreply.github.com>
2022-03-08 14:34:45 +0100

    update lang files

--- UPLOAD/inc/languages/deutsch_du/darkmodeswitch.lang.php
+++ UPLOAD/inc/languages/deutsch_du/darkmodeswitch.lang.php
@@ -1,6 +1,6 @@
 <?php
 
-$l['darkmode'] = "Dunkler Farbmodus (Dark-Mode)";
+$l['darkmode'] = "Dunkler Farbmodus (Dark-Mode):";
 $l['darkmode_auto'] = "Dunklen Farbmodus automatisch erkennen";
 $l['darkmode_enabled'] = "Dunklen Farbmodus immer anwenden";
 $l['darkmode_disabled'] = "Dunklen Farbmodus nie anwenden";
