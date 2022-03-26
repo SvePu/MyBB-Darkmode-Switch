c43f235cbacf5abdb47dd0e70ea9af190458e522
Sven <SvePu@users.noreply.github.com>
2022-03-26 12:11:06 +0100

    added missing lang strings

--- UPLOAD/inc/languages/english/darkmodeswitch.lang.php
+++ UPLOAD/inc/languages/english/darkmodeswitch.lang.php
@@ -10,8 +10,15 @@ $l['setting_darkmodeselector_desc'] = "Should the dark mode selection box be act
 $l['setting_autodarkmodeguests'] = "Automatic DarkMode for Guests";
 $l['setting_autodarkmodeguests_desc'] = "Should the automatic DarkMode be activated for visiting guests?";
 
+$l['setting_default_darkmode'] = "Default setting of the dark mode selection box";
+$l['setting_default_darkmode_desc'] = "Choose the default setting of the dark mode that should be set when registering new members. (default: automatic detection)";
+$l['setting_default_darkmode_2'] = "Automatically detect Dark Mode";
+$l['setting_default_darkmode_1'] = "Always use Dark Mode";
+$l['setting_default_darkmode_0'] = "Never use Dark Mode";
+
 /** UserCP Strings */
 $l['darkmode'] = "Dark Mode:";
+$l['darkmode_desc'] = "Here you can choose whether you want to switch the dark color mode of the forum on or off or whether you want to have it automatically recognized depending on the browser.";
 $l['darkmode_auto'] = "Automatically detect Dark Mode";
 $l['darkmode_enabled'] = "Always use Dark Mode";
 $l['darkmode_disabled'] = "Never use Dark Mode";
